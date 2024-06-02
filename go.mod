2988421424236c567a03db74c693715d17169a08
Paulo H. Nunes <paulohenrique.n.s00@gmail.com>
2024-06-02 01:03:07 -0300

    add initial protection system

--- go.mod
+++ go.mod
@@ -11,5 +11,6 @@ require (
 	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/crypto v0.23.0
 	golang.org/x/sys v0.20.0 // indirect
 )
--- pkg/2clip/addValue.go
+++ pkg/2clip/addValue.go
@@ -22,8 +22,35 @@ var AddCmd = &cobra.Command{
 		db, _ := database.OpenDatabase()
 		defer db.Close()
 
+		if cmd.Flags().Changed("protected") {
+			addProtectedToDatabase(db, key, value)
+			return
+		}
 		addToDatabase(db, key, value)
 	},
+}
+
+var AddProtectedCmd = &cobra.Command{
+	Use:     "protected",
+	Aliases: []string{"-p"},
+	Short:   "Add a protected value to the database",
+	Long:    "Add a protected value to the database based on the provided key.",
+	Args:    cobra.ExactArgs(2),
+	Run: func(cmd *cobra.Command, args []string) {
+		key := args[0]
+		value := args[1]
+
+		db, _ := database.OpenDatabase()
+		defer db.Close()
+
+		addProtectedToDatabase(db, key, value)
+	},
+}
+
+func AddCmdFlags() {
+	AddCmd.Flags().BoolP("protected", "p", true, "Add a protected value to the database")
+
+	AddCmd.AddCommand(AddProtectedCmd)
 }
 
 func addToDatabase(db *bolt.DB, key string, value string) {
@@ -38,6 +65,25 @@ func addToDatabase(db *bolt.DB, key string, value string) {
 		}
 
 		fmt.Printf(`Added "%s" with value "%s"`+"\n", key, value)
+		return nil
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func addProtectedToDatabase(db *bolt.DB, key string, value string) {
+	err := db.Update(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte("2clip"))
+		if bucket == nil {
+			return fmt.Errorf("bucket 2clip not found")
+		}
+
+		err := bucket.Put([]byte(key+" (protected)"), []byte(value))
+		if err != nil {
+			return err
+		}
+		fmt.Printf(`Added "%s" with protect value`+"\n", key)
 		return nil
 	})
 	if err != nil {
