f6c2b5e832fcd3384b0c6a2b4dfd7a5159fd3e95
Paulo H. Nunes <paulohenrique.n.s00@gmail.com>
2024-07-31 20:38:43 -0300

    update: installation script

--- script/install_2clip.sh
+++ script/install_2clip.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
 
 # Define the version you want to install
-VERSION="v1.0.1"
+VERSION="1.0.2"
 
 # Determine the OS and architecture
 OS=$(uname | tr '[:upper:]' '[:lower:]')
 ARCH=$(uname -m)
 
 if [ "$ARCH" = "x86_64" ]; then
-  ARCH="amd64"
+	ARCH="amd64"
+elif [ "$ARCH" = "arm64" ]; then
+	ARCH="arm64"
+else
+	echo "Unsupported architecture: $ARCH"
+	exit 1
 fi
 
 # Determine the download URL
 if [ "$OS" = "linux" ]; then
-  BINARY="2clip-linux-$ARCH"
+	BINARY="2clip-$VERSION-linux-$ARCH"
 elif [ "$OS" = "darwin" ]; then
-  BINARY="2clip-darwin-$ARCH"
+	BINARY="2clip-$VERSION-darwin-$ARCH"
 else
-  echo "Unsupported OS: $OS"
-  exit 1
+	echo "Unsupported OS: $OS"
+	exit 1
 fi
 
 # Download the binary
-URL="https://github.com/Paulooo0/2clip/releases/download/$VERSION/$BINARY"
+URL="https://github.com/Paulooo0/2clip/releases/download/v$VERSION/$BINARY"
 echo "Downloading $BINARY from $URL"
 curl -L -o "$BINARY" "$URL"
 
@@ -31,4 +36,4 @@ chmod +x "$BINARY"
 
 # Move the binary to /usr/local/bin (Linux/macOS) or another directory in PATH
 sudo mv "$BINARY" /usr/local/bin/2clip
-echo "2clip installed to /usr/local/bin/2clip"
+echo "2clip v$VERSION installed to /usr/local/bin/2clip"
