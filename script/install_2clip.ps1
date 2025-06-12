bff75dd505be6a29bd7cd3291f1af881015af6c5
Paulooo0 <paulohenrique.n.s00@gmail.com>
2025-06-12 20:10:09 -0300

    fix: changed app version to current version

--- script/install_2clip.ps1
+++ script/install_2clip.ps1
@@ -1,7 +1,5 @@
-# Define a versão
-$version = "1.3.0"
+$version = "1.3.1"
 
-# Detectar arquitetura
 $arch = $env:PROCESSOR_ARCHITECTURE
 switch ($arch) {
     "AMD64" { $arch = "amd64" }
@@ -13,25 +11,20 @@ switch ($arch) {
     }
 }
 
-# Definir nome do binário
 $binary = "2clip-$version-windows-$arch.exe"
 $url = "https://github.com/Paulooo0/2clip/releases/download/v$version/$binary"
 
-# Download do binário
 $tempPath = "$env:TEMP\$binary"
 Write-Host "Downloading $binary from $url"
 Invoke-WebRequest -Uri $url -OutFile $tempPath
 
-# Caminho de destino (pasta no PATH, por ex: C:\Program Files\2clip)
 $destinationDir = "$env:ProgramFiles\2clip"
 if (!(Test-Path $destinationDir)) {
     New-Item -ItemType Directory -Path $destinationDir | Out-Null
 }
 
-# Mover o binário
 Move-Item -Path $tempPath -Destination "$destinationDir\2clip.exe" -Force
 
-# Adicionar ao PATH (se ainda não estiver)
 $path = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::Machine)
 if ($path -notlike "*$destinationDir*") {
     [System.Environment]::SetEnvironmentVariable("Path", "$path;$destinationDir", [System.EnvironmentVariableTarget]::Machine)
